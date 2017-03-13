da8491c9732a9bb0ec3c7e18141eb7792b290654
Erik Miller <emiller46@radford.edu>
2017-03-13 12:16:26 -0400

    following and unfollowing now work

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -240,6 +240,13 @@ class UserController {
   }
 
   * follow(request, response){
+    const user = yield request.auth.getUser()
+    const following = yield user.followers()
+    for(let i in following){
+      if(following[i].user_id === request.param('user')){
+        response.send(false)
+      }
+    }
     const follower = new Follower()
     follower.user_id = request.param('userId')
     follower.follower = request.param('follower')
@@ -250,9 +257,10 @@ class UserController {
 
   * unfollow(request, response){
     const followerData = yield Database.from('followers').where({user_id: request.param('userId'), follower: request.param('follower')})
-    // follower.is_following = false
-    // yield follower.save()
-    response.send(followerData)
+    const follower = yield Follower.find(followerData[0].id)
+    follower.is_following = false
+    yield follower.save()
+    response.send(follower)
   }
 
 
